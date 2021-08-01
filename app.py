e9502c9acd83aeef44a766885beabdc896db359d
unknown <nasiri.roya@gmail.com>
2021-08-01 14:31:13 +0200

    Register admin blueprint to the main app

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from config import Development
+from mod_admin import admin
 
 app = Flask(__name__)
+app.config.from_object(Development)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Blog Home!'
 
+
+app.register_blueprint(admin)
 
 if __name__ == '__main__':
     app.run()
