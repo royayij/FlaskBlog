13e04f06efc7b14ad5e7557aab62010d7588460d
unknown <nasiri.roya@gmail.com>
2021-08-02 13:03:21 +0200

    Update and add views and add secret key

---
+++ mod_admin/views.py
@@ -0,0 +1,16 @@
+from flask import session
+from . import admin
+
+
+@admin.route('/')
+def index():
+    return 'Hello from admin Index'
+
+
+@admin.route('/login')
+def login():
+    session['name'] = 'Roya'
+    # session.clear()
+    # print(session.get('name'))
+    print(session)
+    return '1'
