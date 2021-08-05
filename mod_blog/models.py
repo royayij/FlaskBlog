126d4e4071b994f91a95a060caecfd233f47b594
unknown <nasiri.roya@gmail.com>
2021-08-05 16:37:44 +0200

    Add views to mod blog

--- mod_blog/models.py
+++ mod_blog/models.py
@@ -1,5 +1,10 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Table
 from app import db
+
+posts_categories = Table('posts_categories', db.metadata,
+                         Column('post_id', Integer, ForeignKey('posts.id', ondelete='cascade')),
+                         Column('category_id', Integer, ForeignKey('categories.id', ondelete='cascade'))
+                         )
 
 
 class Category(db.Model):
@@ -8,6 +13,9 @@ class Category(db.Model):
     description = Column(String(256), nullable=True, unique=False)
     name = Column(String(128), nullable=False, unique=True)
     slug = Column(String(128), nullable=False, unique=True)
+    posts = db.relationship("Post",
+                            secondary=posts_categories,
+                            back_populates='categories')
 
 
 class Post(db.Model):
@@ -17,3 +25,6 @@ class Post(db.Model):
     title = Column(String(128), nullable=False, unique=True)
     content = Column(Text(), nullable=False, unique=False)
     slug = Column(String(128), nullable=False, unique=True)
+    categories = db.relationship("Category",
+                                 secondary=posts_categories,
+                                 back_populates='posts')
