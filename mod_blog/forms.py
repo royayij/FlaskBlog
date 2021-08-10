00351a46874fc6496ffecc04213b08794a57df44
unknown <nasiri.roya@gmail.com>
2021-08-10 11:28:19 +0200

    Update static and utils

--- mod_blog/forms.py
+++ mod_blog/forms.py
@@ -1,29 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, TextAreaField
+from wtforms import TextField, TextAreaField, SelectMultipleField
 from wtforms.validators import DataRequired
+from utils.forms import MultipleCheckBox
 
 
-class CreatePostForm(FlaskForm):
+class PostForm(FlaskForm):
     title = TextField(validators=[DataRequired()])
     summary = TextAreaField()
     content = TextAreaField(validators=[DataRequired()])
     slug = TextField(validators=[DataRequired()])
+    categories = MultipleCheckBox(coerce=int)
 
 
-class ModifyPostForm(FlaskForm):
-    title = TextField(validators=[DataRequired()])
-    summary = TextAreaField()
-    content = TextAreaField(validators=[DataRequired()])
-    slug = TextField(validators=[DataRequired()])
-
-
-class CreateCategoryForm(FlaskForm):
+class CategoryForm(FlaskForm):
     description = TextAreaField()
     name = TextField(validators=[DataRequired()])
     slug = TextField(validators=[DataRequired()])
 
 
-class ModifyCategoryForm(FlaskForm):
-    description = TextAreaField()
-    name = TextField(validators=[DataRequired()])
-    slug = TextField(validators=[DataRequired()])
+class SearchForm(FlaskForm):
+    search_query = TextField(validators=[DataRequired()])
